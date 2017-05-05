0ea92edd1f506cd3c86bb93980b0e0a9e7d5ddab
hluffy <18801613171@163.com>
2017-05-05 10:17:44 +0800

    add ident code

---
+++ src/main/java/com/dk/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.dk.controller;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dk.util.VerifyCodeUtil;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+	@RequestMapping("get.ll")
+	@ResponseBody
+	public void getVerifyCodeImage(HttpServletRequest request, HttpServletResponse response) throws IOException {  
+        //设置页面不缓存  
+        response.setHeader("Pragma", "no-cache");  
+        response.setHeader("Cache-Control", "no-cache");  
+        response.setDateHeader("Expires", 0);  
+        String verifyCode = VerifyCodeUtil.generateTextCode(VerifyCodeUtil.TYPE_NUM_ONLY, 4, null);  
+        //将验证码放到HttpSession里面  
+        request.getSession().setAttribute("verifyCode", verifyCode);  
+        System.out.println("本次生成的验证码为[" + verifyCode + "],已存放到HttpSession中");  
+        //设置输出的内容的类型为JPEG图像  
+        response.setContentType("image/jpeg");  
+        BufferedImage bufferedImage = VerifyCodeUtil.generateImageCode(verifyCode, 90, 30, 9, true, Color.WHITE, Color.BLACK, null);  
+        //写给浏览器  
+        ImageIO.write(bufferedImage, "JPEG", response.getOutputStream());  
+    }  
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<img src="http://localhost:8080/myshiro/login/get.ll">
 </body>
 </html>
